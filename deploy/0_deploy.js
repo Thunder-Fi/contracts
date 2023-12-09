21cae783db3386464b210748b36630e773af295f
br0wnD3v <bothereum@protonmail.com>
2023-12-09 21:12:27 +0530

    Deployment.

--- deploy/0_deploy.js
+++ deploy/0_deploy.js
@@ -2,8 +2,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("Lock", {
+  await deploy("ThunderFi", {
     from: deployer,
-    args: [200000000000],
+    logs: true,
+    args: ["0xaf88d065e77c8cc2239327c5edb3a432268e5831", 6],
   });
 };
