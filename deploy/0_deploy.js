b7d32f29683cdc7ae52ffa175d9aa4a93b0ca739
br0wnD3v <bothereum@protonmail.com>
2023-12-09 23:37:48 +0530

    Minor Updates.

--- deploy/0_deploy.js
+++ deploy/0_deploy.js
@@ -1,36 +1,27 @@
 module.exports = async ({ getNamedAccounts, deployments, ethers }) => {
   const { deploy } = deployments;
-  const { deployer, seller, purchaser } = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
 
-  const provider = ethers.provider;
-  const signerDeployer = provider.getSigner(deployer);
-  // const signerSeller = provider.getSigner(seller);
-  // const signerPurchaser = provider.getSigner(purchaser);
+  console.log("\n");
 
   const MockUSDC = await deploy("MockToken", {
     from: deployer,
+    log: true,
     args: ["USD Coin", "USDC"],
   });
-  console.log("\nDeployed USDC Mocks at   :", MockUSDC.address);
+  console.log("Deployed USDC at :", MockUSDC.address, "\n");
 
   const MockUSDT = await deploy("MockToken", {
     from: deployer,
+    log: true,
     args: ["USD Tether", "USDT"],
   });
-  console.log("Deployed USDT at   :", MockUSDT.address);
+  console.log("Deployed USDT at :", MockUSDT.address, "\n");
 
-  const mockUsdc = new ethers.Contract(
-    MockUSDC.address,
-    MockUSDC.abi,
-    signerDeployer
-  );
-  await mockUsdc.mint(deployer);
-  await mockUsdc.mint(seller);
-  await mockUsdc.mint(purchaser);
-
-  await deploy("ThunderFi", {
+  const TF = await deploy("ThunderFi", {
     from: deployer,
     log: true,
     args: [MockUSDC.address, 6],
   });
+  console.log("Deployed ThunderFi at :", TF.address, "\n");
 };
